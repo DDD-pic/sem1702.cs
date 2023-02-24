d55b35ca64433ad459e142c61788757e8cf1f6c2
DDD-pic <sveta.moroz2010@yandex.ru>
2023-02-24 11:56:21 +0300

    zadacha

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 //12821 -> да
 //23432 -> да
   
-/*
+
 Console.WriteLine("Введите пятизначное число: ");
 string number = Console.ReadLine();
 int len = number.Length;
@@ -24,14 +24,14 @@ else
 {
     Console.WriteLine($" {number} - не является пятизначным");
 }
-*/
+
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
 //A (3,6,8); B (2,1,-7), -> 15.84
 
 //A (7,-5, 0); B (1,-1,9) -> 11.53
   
-/*
+
 Console.Write("Введите коорддинату точки A по оси X: ");
 int x1 =  Convert.ToInt32(Console.ReadLine());
 
@@ -53,9 +53,12 @@ int z2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 -y1,2) + Math.Pow(z2 - z1,2));
 Console.WriteLine(Math.Round(result,2));
-*/
 
 
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
 
    int ReadInt(string message)
 {
